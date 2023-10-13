5d6e82d2c753298543d68a00610b843fb2ae0467
RupkumarBera <78893621+RupkumarBera@users.noreply.github.com>
2023-10-13 08:15:22 +0530

    Add files via upload

---
+++ C-Programming/Test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("Rupkumar bera\n");
+    int n;
+    scanf("%d",&n);
+    printf("The number is = %d\n",n);
+    return 0;
+}
