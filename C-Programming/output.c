8b6be2e5c5a7781fbff0daa61d762972a3589228
RupkumarBera <78893621+RupkumarBera@users.noreply.github.com>
2023-11-08 09:34:25 +0530

    Add files via upload

---
+++ C-Programming/output.c
@@ -0,0 +1,9 @@
+# include<stdio.H>
+
+int main (){
+    printf ("          1\n");
+    printf("       2       3\n");
+    printf("    4      5      6\n");
+    printf("  7     8      9     10");
+    return 0;
+}
