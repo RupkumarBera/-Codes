345070162ea509cfbd1c4c6edc32a0ec0d55c83d
Rupkumar Bera <78893621+RupkumarBera@users.noreply.github.com>
2024-08-28 23:59:26 +0530

    Add files via upload

---
+++ pattern/Floydtri.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, num = 1;
+
+    // Number of rows for Floyd's Triangle
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
